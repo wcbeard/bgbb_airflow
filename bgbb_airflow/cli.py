3be3f525c0c930b1674f29e25f6872ac31d1d04b
wcbeard <wcbeard10@gmail.com>
2019-02-13 14:07:02 -0500

    cli.py

---
+++ bgbb_airflow/cli.py
@@ -0,0 +1,22 @@
+import click
+
+from bgbb_airflow import (
+    fit_airflow_job as bgbb_fit_job,
+    pred_airflow_job as bgbb_pred_job,
+)
+
+print("in cli.py")
+
+
+@click.group()
+def entry_point():
+    pass
+
+
+entry_point.add_command(bgbb_fit_job.main, "bgbb_fit")
+entry_point.add_command(bgbb_pred_job.main, "bgbb_pred")
+
+
+if __name__ == "__main__":
+    # main(auto_envvar_prefix='MOZETL')
+    entry_point()
